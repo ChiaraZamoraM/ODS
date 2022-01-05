3b1cd893545db46bdf8f70ab91ffb1b2788042db
ChiaraZamoraM <45526251+ChiaraZamoraM@users.noreply.github.com>
2022-01-04 23:27:39 -0500

    Cambios en RDS

--- ODS/app.R
+++ ODS/app.R
@@ -15,7 +15,7 @@ library(rio)
 
 setwd("~/GitHub/ODS")
 
-subset1_1_1 = import("https://github.com/ChiaraZamoraM/ODS/raw/main/subset_1_1_1.RDS")
+ODSSpr = import("https://github.com/ChiaraZamoraM/ODS/raw/main/ODSSpr.RDS")
 
 ui <- fluidPage(
 
@@ -27,16 +27,18 @@ ui <- fluidPage(
             tags$a(href="http://ods.inei.gob.pe/ods/objetivos-de-desarrollo-sostenible", 
                    "Referencia de los datos",
                    target= "_blank"),
-                   h5("Todos los datos"),
+                   h5("Este es un Shiny App de prueba."),
             selectInput("ano",
                         "Seleccione una fecha",
-                        choices = unique(subset1_1_1$Ano)
+                        choices = unique(ODSSpr$Ano)
             )
             ),
         mainPanel(
             tabsetPanel(
                 tabPanel("1.1.1",leafletOutput("pobrezaex")),
-                tabPanel("1.2.1",leafletOutput("pobrezatot"))
+                tabPanel("1.2.1",leafletOutput("pobrezatot")),
+                tabPanel("1.3.1",leafletOutput("pension")),
+                tabPanel("1.4.1",leafletOutput("servbasicos"))
                 )
             )
         )
@@ -46,24 +48,100 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-     ano_subset1_1_1 <- reactive({
-        y    <- subset1_1_1 %>% filter(Ano == input$ano)
+     ano_ODSSpr <- reactive({
+        y    <- ODSSpr %>% filter(Ano == input$ano)
         return(y)
      })
      
      output$pobrezaex = renderLeaflet({
-         pal1 = colorNumeric(palette = "Blues", domain = subset1_1_1$Valor)
+         pal1 = colorNumeric(palette = "heat", domain = ano_ODSSpr$`1_1_1`)
          
-         ano_subset1_1_1()  %>% 
+         ano_ODSSpr()  %>% 
              st_transform(crs= "+init=epsg:4326") %>%
              leaflet() %>%
              addProviderTiles(provider= "CartoDB.Positron") %>%
-             addPolygons(label= ano_subset1_1_1()$DEPARTAMEN,
+             addPolygons(label= ano_ODSSpr()$DEPARTAMEN,
                          stroke = FALSE, 
                          smoothFactor =  .5,
                          opacity = 1,
                          fillOpacity = 0.7,
-                         fillColor = ~pal1(ano_subset1_1_1()$Valor),
+                         fillColor = ~pal1(ano_ODSSpr()$`1_1_1`),
+                         highlightOptions = highlightOptions(weight = 5,
+                                                             fillOpacity= 1,
+                                                             color = "black",
+                                                             opacity = 1,
+                                                             bringToFront = TRUE))%>%
+             addLegend("bottomright",
+                       pal = pal1,
+                       values = ~`1_1_1`,
+                       title= "Porcentaje (%)",
+                       opacity= 0.7) %>%
+             addControl("Incidencia de la pobreza extrema", 
+                        position = "topleft", className="map-title")
+     })
+     output$pobrezatot = renderLeaflet({
+         pal1 = colorNumeric(palette = "heat", domain = ODSSpr$`1_2_1`)
+         
+         ano_ODSSpr()  %>% 
+             st_transform(crs= "+init=epsg:4326") %>%
+             leaflet() %>%
+             addProviderTiles(provider= "CartoDB.Positron") %>%
+             addPolygons(label= ano_ODSSpr()$DEPARTAMEN,
+                         stroke = FALSE, 
+                         smoothFactor =  .5,
+                         opacity = 1,
+                         fillOpacity = 0.7,
+                         fillColor = ~pal1(ano_ODSSpr()$Valor),
+                         highlightOptions = highlightOptions(weight = 5,
+                                                             fillOpacity= 1,
+                                                             color = "black",
+                                                             opacity = 1,
+                                                             bringToFront = TRUE))%>%
+             
+             addLegend("bottomright",
+                       pal = pal1,
+                       values = ~Valor,
+                       title= "Porcentaje (%)",
+                       opacity= 0.7) 
+     })
+     output$pension = renderLeaflet({
+         pal1 = colorNumeric(palette = "heat", domain = ODSSpr$`1_3_1`)
+         
+         ano_ODSSpr()  %>% 
+             st_transform(crs= "+init=epsg:4326") %>%
+             leaflet() %>%
+             addProviderTiles(provider= "CartoDB.Positron") %>%
+             addPolygons(label= ano_ODSSpr()$DEPARTAMEN,
+                         stroke = FALSE, 
+                         smoothFactor =  .5,
+                         opacity = 1,
+                         fillOpacity = 0.7,
+                         fillColor = ~pal1(ano_ODSSpr()$Valor),
+                         highlightOptions = highlightOptions(weight = 5,
+                                                             fillOpacity= 1,
+                                                             color = "black",
+                                                             opacity = 1,
+                                                             bringToFront = TRUE))%>%
+             
+             addLegend("bottomright",
+                       pal = pal1,
+                       values = ~Valor,
+                       title= "Porcentaje (%)",
+                       opacity= 0.7) 
+     })
+     output$servbasicos = renderLeaflet({
+         pal1 = colorNumeric(palette = "heat", domain = ODSSpr$`1_4_1`)
+         
+         ano_ODSSpr()  %>% 
+             st_transform(crs= "+init=epsg:4326") %>%
+             leaflet() %>%
+             addProviderTiles(provider= "CartoDB.Positron") %>%
+             addPolygons(label= ano_ODSSpr()$DEPARTAMEN,
+                         stroke = FALSE, 
+                         smoothFactor =  .5,
+                         opacity = 1,
+                         fillOpacity = 0.7,
+                         fillColor = ~pal1(ano_ODSSpr()$Valor),
                          highlightOptions = highlightOptions(weight = 5,
                                                              fillOpacity= 1,
                                                              color = "black",
